2f7cdd3e1f7a11e17ee2dea102bb2559d3645254
tantanGH <tantangh68@gmail.com>
2023-05-01 11:14:28 +0900

    update

--- ureversi/ureversi.py
+++ ureversi/ureversi.py
@@ -2,11 +2,10 @@ import x68k
 import random
 import time
 import sys
-from uctypes import addressof
 from struct import pack
 
 # バージョン
-VERSION = const("2023.04.30")
+VERSION = const("2023.05.01")
 
 # ボードクラス
 class Board:
@@ -21,6 +20,10 @@ class Board:
   # クラス変数: 1マスの横幅、縦幅
   GRID_WIDTH  = const(50)
   GRID_HEIGHT = const(60)
+
+  # 8方向チェック用オフセット
+  DX = [  0,  1,  1,  1,  0, -1, -1, -1 ]
+  DY = [ -1, -1,  0,  1,  1,  1,  0, -1 ]
 
   # コンストラクタ
   def __init__(self, board_pos, bitmap_black, bitmap_white):
@@ -83,15 +86,97 @@ class Board:
           self.board[ i + j * 8 ] = 2
 
   # 駒を置けるかどうかのチェック
-  def is_placeable(self, pos, color):
-    if self.board[ pos[0] + pos[1] * 8 ] == 0:
-      return True
-    else:
-      return False
+  def get_placeable_directions(self, pos, color):
+
+    # 置ける方向リスト
+    directions = []
+
+    # 既に何か置いてあったら全然ダメ
+    if self.board[ pos[0] + pos[1] * 8 ] != 0:
+      return directions   # 空リスト
+
+    # 8方向確認
+    for i in range(8):
+
+      # チェック開始基準点
+      cx = pos[0]
+      cy = pos[1]
+
+      # チェック用キューイング
+      cs = []
+      while True:
+
+        # チェック対象位置
+        cx += Board.DX[i]
+        cy += Board.DY[i]
+
+        # 盤外ならストップ
+        if cx < 0 or cx > 7 or cy < 0 or cy > 7:
+          break   
+
+        # 未表示かもしれないので絶対値で
+        c = abs(self.board[ cx + cy * 8 ])
+
+        # 何も置いてなければストップ
+        if c == 0:
+          break
+
+        # キューに追加
+        cs.append(c)
+
+      # キューを先頭から見て [ 自分以外の色が1個以上 + 自分の色 ] となっていれば置ける
+      cc = False
+      rc = False
+      while len(cs) > 0:
+        ct = cs.pop(0)
+        if ct == color:           # 自分の色を見つけたら終了
+          rc = cc                 # 既に自分以外の色を見つけているかで判定
+          break
+        else:
+          cc = True               # 自分以外の色を見つけた
+
+      # この方向に置けるのであれば、置ける方向リストに追加
+      if rc:
+        directions.append(i)
+
+    return directions
 
   # 駒を置く
   def place(self, pos, color):
+
+    # 置けたかどうか
+    rc = False
+
+    # 置ける方向のチェック
+    directions = self.get_placeable_directions(pos, color)
+    if len(directions) == 0:
+      return rc
+
+    # 置く
     self.board[ pos[0] + pos[1] * 8 ] = (-1) * color
+
+    # ひっくり返す
+    for i in directions:
+      cx = pos[0]
+      cy = pos[1]
+      while True:
+        cx += Board.DX[i]
+        cy += Board.DY[i]
+        if cx < 0 or cx > 7 or cy < 0 or cy > 7:
+          break
+        c = abs(self.board[ cx + cy * 8 ])
+        if c == color:
+          break
+        self.board[ cx + cy * 8 ] = (-1) * color
+        self.repaint()
+
+    # 盤面再描画
+#    self.repaint()
+
+    # 置けたよ
+    rc = True
+
+    return rc
 
   # 駒を数える
   def count(self):
@@ -187,13 +272,72 @@ class Cursor:
     self.pos_y = ( self.pos_y + 1 ) % 8
     self.scroll()
 
+# 指定サイズ・指定位置メモリ上イメージの表示
+def put_image(pos, size, bitmap_image):
+  gvram = x68k.GVRam()
+  gvram.put(pos[0], pos[1], pos[0] + size[0] - 1, pos[1] + size[1] - 1, bitmap_image)
+
 # 512x512x65536イメージファイルの表示
-def load_image(image_file):
+def load_full_image(image_file):
   with open(image_file, "rb") as f:
+    
+    # 一旦グラフィックOFF
+    b = x68k.iocs(x68k.i.B_WPEEK, a1=0xe82600) & 0xffe0
+    x68k.vsync()
+    x68k.iocs(x68k.i.B_WPOKE, a1=0xe82600, d1=b)
+
+    # ファイルの内容をGVRAMに転送
     gvram = x68k.GVRam()
     for y in range(0, 512, 64):
       line_data = f.read(512 * 2 * 64)
       gvram.put(0, y, 511, y + 63, line_data)
+
+    # グラフィックON
+    x68k.vsync()
+    x68k.iocs(x68k.i.B_WPOKE, a1=0xe82600, d1=(b | 0x0f))  
+
+# 342x512x65536イメージファイルの縦スクロール表示
+def load_portrait_image(image_file):
+  with open(image_file, "rb") as f:
+    
+    # 384x256x65536モード (IPL-ROM 1.6 or CRTMOD16.X)
+    x68k.crtmod(31, True)
+
+    # 表示領域外にも描けるようにクリッピング領域を広げる
+    x68k.iocs(x68k.i.WINDOW, d1=0, d2=0, d3=511, d4=511)
+
+    # 一旦グラフィックOFF
+    b = x68k.iocs(x68k.i.B_WPEEK, a1=0xe82600) & 0xffe0
+    x68k.vsync()
+    x68k.iocs(x68k.i.B_WPOKE, a1=0xe82600, d1=b)
+
+    # ファイルの内容をGVRAMに転送
+    gvram = x68k.GVRam()
+    for y in range(0, 512, 64):
+      line_data = f.read(342 * 2 * 64)
+      gvram.put(22, y, 363, y + 63, line_data)
+
+    # 初期スクロール位置
+    x68k.iocs(x68k.i.SCROLL, d1=0, d2=0, d3=256)
+    x68k.iocs(x68k.i.SCROLL, d1=1, d2=0, d3=256)
+    x68k.iocs(x68k.i.SCROLL, d1=2, d2=0, d3=256)
+    x68k.iocs(x68k.i.SCROLL, d1=3, d2=0, d3=256)
+
+    # グラフィックON
+    x68k.vsync()
+    x68k.iocs(x68k.i.B_WPOKE, a1=0xe82600, d1=(b | 0x0f))  
+
+    # 一呼吸置いて
+    time.sleep(2.000)
+
+    # お楽しみ縦スクロール
+    for i in range(256):
+      y = 255 - i
+      x68k.vsync()
+      x68k.iocs(x68k.i.SCROLL, d1=0, d2=0, d3=y)
+      x68k.iocs(x68k.i.SCROLL, d1=1, d2=0, d3=y)
+      x68k.iocs(x68k.i.SCROLL, d1=2, d2=0, d3=y)
+      x68k.iocs(x68k.i.SCROLL, d1=3, d2=0, d3=y)
 
 # メイン
 def main():
@@ -214,16 +358,17 @@ def main():
   bitmap_white = None
   with open("koma_w.dat", "rb") as f:
     bitmap_white = f.read()
-  if bitmap_black is None or bitmap_white is None:
-    print("error: data load error.")
-    sys.exit(1)
 
+  # COM顔ビットマップデータのロード
+  bitmap_com1 = None
+  with open("com1.dat", "rb") as f:
+    bitmap_com1 = f.read()
 
   # メインループ
   abort = False
   while abort is False:
 
-    # タイトル画面
+    # タイトル画面 -------------------------------------------------------------------------------
 
     # 512 x 512 x 65536 (31kHz) mode
     x68k.vsync()
@@ -234,7 +379,7 @@ def main():
     x68k.iocs(x68k.i.B_WPOKE, a1=0xe82500, d1=0b00000110_11100100)
 
     # タイトルビットマップ表示
-    load_image("title3.dat")
+    load_full_image("title3.dat")
 
     # タイトル文字表示
     print("\x1b[25;25H\x1b[37mPUSH SPACE KEY\x1b[m", end="")
@@ -253,7 +398,17 @@ def main():
       break
 
 
-    # ゲーム画面
+    # ゲーム画面 -------------------------------------------------------------------------------
+
+    # 先手後手
+    if random.randint(0,1) == 0:
+      # COMが先手(黒)
+      color_computer = 1
+      color_player = 2
+    else:
+      # COMが後手(白)
+      color_computer = 2
+      color_player = 1
 
     # 512 x 512 x 65536 (31kHz) mode
     x68k.vsync()
@@ -263,12 +418,17 @@ def main():
     x68k.vsync()
     x68k.iocs(x68k.i.B_WPOKE, a1=0xe82500, d1=0b00001001_11100100)
 
+    # 一旦グラフィック・テキスト・スプライトOFF
+    b = x68k.iocs(x68k.i.B_WPEEK, a1=0xe82600) & 0xff00
+    x68k.vsync()
+    x68k.iocs(x68k.i.B_WPOKE, a1=0xe82600, d1=b)
+
     # ボード初期化
-    board = Board((0, 0), bitmap_black, bitmap_white)
+    board = Board((0, 4), bitmap_black, bitmap_white)
     board.repaint()
 
     # カーソル初期化
-    cursor = Cursor((0, 0), (0, 0))
+    cursor = Cursor((0, 4), (0, 0))
     cursor.scroll()
 
     # インフォメーション表示
@@ -276,59 +436,170 @@ def main():
     print("\x1b[3;53H\x1b[mPRO-68K     \x1b[m", end="")
 
     print("\x1b[6;53H\x1b[37mCOMPUTER\x1b[m", end="")
-    print("\x1b[8;53H白(後手)", end="")
-    print("\x1b[10;53H2枚", end="")
-    
-    print("\x1b[14;53H\x1b[37mPLAYER\x1b[m", end="")
-    print("\x1b[16;53H黒(先手)", end="")
+    if color_computer == 1:
+      print("\x1b[16;53H黒(先手)", end="")
+    else:
+      print("\x1b[16;53H白(後手)", end="")
     print("\x1b[18;53H2枚", end="")
+    
+    print("\x1b[22;53H\x1b[37mPLAYER\x1b[m", end="")
+    if color_player == 1:
+      print("\x1b[24;53H黒(先手)", end="")
+    else:
+      print("\x1b[24;53H白(後手)", end="")      
+    print("\x1b[26;53H2枚", end="")
 
-    print("\x1b[22;53Hカーソルキー", end="")
-    print("\x1b[23;53Hの上下左右で", end="")
-    print("\x1b[24;53Hマスを選択し", end="")
-    print("\x1b[25;53Hリターンで駒", end="")
-    print("\x1b[26;53Hを置きます", end="")
-    print("\x1b[29;53HESCで退出", end="")
+    print("\x1b[32;1H[←↑↓→]:マス選択 [RET/SP]:駒を置く [p]:パス [ESC]:終了", end="")
+
+    # COMビットマップ表示
+    put_image((408, 104), (104, 124), bitmap_com1)
+
+    # グラフィック・テキスト・スプライトON
+    x68k.vsync()
+    x68k.iocs(x68k.i.B_WPOKE, a1=0xe82600, d1=(b | 0x6f))
 
     # ゲームループ
-    abort = False
-    while abort is False:
+    game_end = False
+    color_active = 1
+    while game_end is False and abort is False:
 
-      # キーボードでカーソルを移動させスペースキーまたはリターンキーで確定
-      while True:
-        if x68k.iocs(x68k.i.B_KEYSNS):
-          scan_code = ( x68k.iocs(x68k.i.B_KEYINP) >> 8 ) & 0x7f
-          if scan_code == 0x01:       # esc key
-            abort = True
-            break
-          elif scan_code == 0x3b:     # left key
-            cursor.move_left()
-          elif scan_code == 0x3d:     # right key
-            cursor.move_right()
-          elif scan_code == 0x3c:     # up key
-            cursor.move_up()
-          elif scan_code == 0x3e:     # down key
-            cursor.move_down()
-          elif scan_code == 0x35:     # space key
-            break
-          elif scan_code == 0x1d:     # return key
+      computer_placed = False
+      player_placed = False
+
+      if color_active == color_computer:
+      
+        # COMの手番
+
+        # 試行順をシャッフル
+        pos = list(range(64))
+        for i in range(100):
+          a = random.randint(0, 63)
+          b = random.randint(0, 63)
+          pos[ a ], pos[ b ] = pos[ b ], pos[ a ]
+
+        for p in pos:
+          
+          # 置けるか？
+          if board.place((p % 8, p // 8), color_computer):
+
+            # 置けた
+            computer_placed = True
             break
 
-      # ESCキーが押された？
-      if abort:
+      else:
+        
+        # 自分の手番
+        while player_placed is False and abort is False:
+
+          # キーボードでカーソルを移動させスペースキーまたはリターンキーで確定
+          while True:
+            if x68k.iocs(x68k.i.B_KEYSNS):
+              scan_code = ( x68k.iocs(x68k.i.B_KEYINP) >> 8 ) & 0x7f
+              if scan_code == 0x01:       # esc key
+                abort = True
+                break
+              elif scan_code == 0x3b:     # left key
+                cursor.move_left()
+              elif scan_code == 0x3d:     # right key
+                cursor.move_right()
+              elif scan_code == 0x3c:     # up key
+                cursor.move_up()
+              elif scan_code == 0x3e:     # down key
+                cursor.move_down()
+              elif scan_code == 0x35:     # space key
+                color_player = 2
+                break
+              elif scan_code == 0x1d:     # return key
+                color_player = 1
+                break
+              elif scan_code == 0x1a:     # p key
+                break
+
+          # ESCキーが押された？
+          if abort:
+            break
+
+          # カーソル位置に本当における？
+          if board.place((cursor.pos_x, cursor.pos_y), color_player):
+
+            # 置けた
+            player_placed = True
+            break
+
+      # 置けた場合
+      if computer_placed or player_placed:
+
+        # 枚数表示更新
+        counts = board.count()
+        if color_computer == 1:
+          print(f"\x1b[18;53H{counts[0]}枚 ", end="")
+          print(f"\x1b[26;53H{counts[1]}枚 ", end="")
+        else:
+          print(f"\x1b[18;53H{counts[1]}枚 ", end="")
+          print(f"\x1b[26;53H{counts[0]}枚 ", end="")
+
+        # パーフェクトゲーム？
+        if counts[0] == 0 or counts[1] == 0:
+          game_end = True
+          break          
+
+        # もう置く場所ない？
+        if counts[0] + counts[1] == 64:
+          game_end = True
+          break
+
+        # 手番交代
+        if color_active == 1:
+          color_active = 2
+        else:
+          color_active = 1
+
+      else:
+
+        # どちらも置けなかったら終了
+        game_end = True
         break
 
-      # カーソル位置に本当における？
-      if board.is_placeable((cursor.pos_x, cursor.pos_y), 1):
-        board.place((cursor.pos_x, cursor.pos_y), 1)
-        board.repaint()
-        counts = board.count()
-        print(f"\x1b[10;53H{counts[1]}枚 ", end="")
-        print(f"\x1b[18;53H{counts[0]}枚 ", end="")
+    # 1ゲーム終了
+    if abort is False:
 
+      # 結果表示
+      computer_win = None
+      counts = board.count()
+      if counts[0] > counts[1]:
+        computer_win = (color_computer == 1)
+        print("\x1b[32;1H黒の勝ちです\x1b[K", end="")
+      elif counts[0] < counts[1]:
+        computer_win = (color_computer == 2)
+        print("\x1b[32;1H白の勝ちです\x1b[K", end="")
+      else:
+        print("\x1b[32;1H引き分けです\x1b[K", end="")      
+
+      # 3秒待つ
+      time.sleep(3.000)
+
+      # ビジュアルシーン
+      if computer_win is None:
+        # 引き分けなら何もなし
+        pass
+      elif computer_win:
+        # COMの勝ち
+        load_portrait_image(f"win{random.randint(1,3)}.dat")
+      else:
+        # COMの負け
+        load_portrait_image(f"lose{random.randint(1,3)}.dat")
+
+      # 文字表示
+      print("\x1b[2;2H\x1b[37mPUSH ANY KEY\x1b[m", end="")
+
+      # キー待ち
+      while True:
+        scan_code = ( x68k.iocs(x68k.i.B_KEYINP) >> 8 ) & 0x7f
+        if scan_code != 0:
+          break
 
   # 終了処理
-
+  
   # flush key buffer
   x68k.dos(x68k.d.KFLUSH,pack('h',0))  
 
